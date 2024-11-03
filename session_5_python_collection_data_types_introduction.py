c8deaf945c4d972049f6301ef1098cc14531d5cf
riki-graha <riki.graha@gmail.com>
2024-11-03 16:24:08 +0700

    session_3_python_looping_statements_part_2

---
+++ session_5_python_collection_data_types_introduction.py
@@ -0,0 +1,17 @@
+# Collection Data Types Introduction
+
+# Tipe Data List
+listContoh = ['hello', 1, 1, 3, True]
+print(listContoh, type(listContoh))  # Output: ['hello', 1, 1, 3, True] <class 'list'>
+
+# Tipe Data Tuple
+tupleContoh = ('hello', 1, 1, 3, True)
+print(tupleContoh, type(tupleContoh))  # Output: ('hello', 1, 1, 3, True) <class 'tuple'>
+
+# Tipe Data Set
+setContoh = {'hello', 1, 1, 3, True}
+print(setContoh, type(setContoh))  # Output: {1, 3, 'hello'} <class 'set'>
+
+# Tipe Data Dictionary
+dictContoh = {'nama': 'Ricky', 'usia': 25}
+print(dictContoh, type(dictContoh))  # Output: {'nama': 'Ricky', 'usia': 25} <class 'dict'>
