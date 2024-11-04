51b7065a94c78cd21d35b431cf01a83391f57641
riki-graha <riki.graha@gmail.com>
2024-11-04 21:19:57 +0700

    session_6_python_function_without_input_and_output

---
+++ session_6_python_function_without_input_and_output.py
@@ -0,0 +1,8 @@
+# Definisi fungsi tanpa input dan output
+def salam():
+    print("Hello selamat datang di Purwadhika!")
+    print("Semoga hari anda menyenangkan!")
+
+# Pemanggilan fungsi
+salam()
+salam()
