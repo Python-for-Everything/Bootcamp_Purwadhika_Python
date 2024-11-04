cbda52f5d803033f8c1022f6bb88a4adaf5c015f
riki-graha <riki.graha@gmail.com>
2024-11-04 21:29:43 +0700

    session_6_python_function_with_input_and_output

---
+++ session_6_python_function_with_input_and_output.py
@@ -0,0 +1,11 @@
+# Definisi fungsi dengan input dan output
+def tambah(angka1, angka2):
+    # Mengembalikan hasil penjumlahan angka1 dan angka2
+    return angka1 + angka2
+
+# Memanggil fungsi dan menyimpan hasilnya dalam variabel
+hasil1 = tambah(5, 4)
+print(hasil1)  # Output: 9
+
+# Memanggil fungsi dan langsung mencetak hasilnya
+print(tambah(10, 2))  # Output: 12
